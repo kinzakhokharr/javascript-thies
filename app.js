67a8e644d0cb5581fcdcf2368d64ef2c3eab2170
kinzakhokharr <kinzakhokharr@gmail.com>
2024-08-07 04:48:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,114 @@
+
+// it is a printing statemet
+
+/*
+jhdjashdhjasdjasjdasdkjaksjd
+dkshdjksjdkl
+djhdjsjd
+*/
+
+
+
+// let name = "Kinza";
+// name = "shanzayyy"
+// console.log(name);
+
+
+
+// let count = 1000
+// let isPresent = true
+
+// const firstName = "Kinza";
+// firstName = "Shanza"
+// console.log(firstName)
+
+
+// console.log(typeof(count))
+// console.log(count)
+// console.log(typeof(isPresent))
+
+// let count = 34
+
+// {
+//  let count = 40
+// console.log(count)
+
+// }
+// console.log(count)
+
+
+// function xyz(){
+//     let x = 40
+// console.log(x);
+// }
+
+
+// xyz()
+
+
+
+
+
+
+
+
+
+
+// let x = 100
+// let y = 20
+
+// console.log( x + y)
+// console.log( x * y)
+// console.log( x / y)
+// console.log( x - y)
+// console.log( x > y)
+
+
+// Array
+
+// Indexing       0       1      2         3         4            5        
+// let students = ["Akash","Zain", "Moeen", "jashwant", "Abu bakr", "Maneela"]
+// let indices = students[2,4,6]
+
+
+// console.log(students[5][4])
+
+
+
+
+// let students = {
+
+//     name: "Maneela",
+//     age:20,
+//     email:"maneela@gmail.com",
+    
+// }
+
+// console.log(students.name)
+
+
+
+
+
+  
+ function myApp(a,b){
+    return a+b
+ }
+
+ console.log(myApp(8479237428,100))
+// myApp()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
